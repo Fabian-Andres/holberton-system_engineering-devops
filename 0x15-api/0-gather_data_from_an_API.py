04a5fb1ecc8b978ade4610fe1a75f81e346d9f53
Fabian Andres <f4bian.andres@gmail.com>
2020-09-14 21:22:04 +0000

    [feature] Task 0 - Funtion to gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,44 +2,48 @@
 """
 Funtion to gather data from an API
 """
+import json
 import sys
 import requests
-import json
 
 
 if __name__ == "__main__":
     # Check if number is ingeter
-    def is_integer_num(n):
-        if isinstance(n, int):
-            return True
-        if isinstance(n, float):
-            return n.is_integer()
-        return False
+    def is_integer(n):
+        try:
+            float(n)
+        except ValueError:
+            return False
+        else:
+            return float(n).is_integer()
 
-    # URL's to get all information
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-    url_tasks = ('https://jsonplaceholder.typicode.com/users/'
-                 + sys.argv[1] + '/todos')
+    n_id = sys.argv[1]
 
-    # Get data response whit requests
-    r_user = requests.get(url_user)
-    r_tasks = requests.get(url_tasks)
+    if is_integer(n_id):
+        # URL's to get all information
+        url = 'https://jsonplaceholder.typicode.com'
+        url_user = (url + '/users/' + n_id)
+        url_tasks = (url + '/users/' + n_id + '/todos')
 
-    # Set data response to json
-    obj_user = json.loads(r_user.text)
-    obj_task = json.loads(r_tasks.text)
+        # Get data response whit requests
+        r_user = requests.get(url_user)
+        r_tasks = requests.get(url_tasks)
 
-    task_done = 0
-    TASK_TITLE = ""
-    for task in obj_task:
-        TASK_TITLE += "\t" + task["title"] + "\n"
-        if task["completed"] is True:
-            task_done += 1
+        # Set data response to json
+        obj_user = json.loads(r_user.text)
+        obj_task = json.loads(r_tasks.text)
 
-    NUMBER_OF_DONE_TASKS = 'a'
-    EMPLOYEE_NAME = obj_user["name"]
-    NUMBER_OF_DONE_TASKS = task_done
-    TOTAL_NUMBER_OF_TASKS = len(obj_task)
-    print("Employee %s is done with tasks(%d/%d):\n %s" %
-          (EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
-           TOTAL_NUMBER_OF_TASKS, TASK_TITLE), end="")
+        task_done = 0
+        TASK_TITLE = ""
+        for task in obj_task:
+            TASK_TITLE += "\t" + task["title"] + "\n"
+            if task["completed"] is True:
+                task_done += 1
+
+        EMPLOYEE_NAME = obj_user["name"]
+        NUMBER_OF_DONE_TASKS = task_done
+        TOTAL_NUMBER_OF_TASKS = len(obj_task)
+        print("Employee %s is done with tasks(%d/%d):"
+              .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
+                      TOTAL_NUMBER_OF_TASKS))
+        print(TASK_TITLE, end="")
