baa54d2624aaad5cbc91bf81ad7409c1bf1e316f
Fabian Andres <f4bian.andres@gmail.com>
2020-09-30 21:58:58 +0000

    [feature] rename phpp file to php

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Puppet function to rename phpp file to php
 exec { 'Fix phpp file':
-    command => "'sed -i 's|phpp|php|' /var/www/html/wp-settings.php",
+    command => "sed -i 's|phpp|php|' /var/www/html/wp-settings.php",
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:'
 }
