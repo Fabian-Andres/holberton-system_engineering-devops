370c157460e4d0d1c62a3d86c1fa5b402cc909d2
Fabian Andres <f4bian.andres@gmail.com>
2020-10-09 01:09:48 +0000

    Sky is the limit, let's bring that limit higher and Readme

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # 0. Sky is the limit, let's bring that limit higher
 
-exec {'fix nginx':
+exec {'fix nginx server':
     command => "sed -i 's/15/1000/g' /etc/default/nginx",
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
 exec {'restart nginx':
     command => 'service nginx restart',
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
